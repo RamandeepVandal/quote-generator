217fb8bd4f388a3ec17d3d424ba14d3f476ae324
RamandeepVandal <ramandeepvandal@gmail.com>
2022-09-12 18:58:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // quote
+  let [quote, setQuote] = useState("Important principles may, and must, be inflexible.");
+
+  // API KEY
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  // fetch api data
+  const generateQuote = () => {
+
+    // random quote 
+    let rand = Math.floor(Math.random() * 1600) + 1;
+
+    fetch(API_KEY)
+      .then((response) => { return response.json(); })
+      .then((data) => { setQuote(quote = data[rand].text); });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <h3>{quote}</h3>
+        <button onClick={generateQuote}>Need Inspiration</button>
+      </div>
     </div>
   );
 }
